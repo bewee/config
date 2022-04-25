c0ca008bb839a098b82959187f309457ecec49dd
Benedikt Weber <benedikt.web23@gmail.com>
2022-04-25 20:08:31 +0200

    Improved bufferline

--- nvim/lua/plugin-bufferline.lua
+++ nvim/lua/plugin-bufferline.lua
@@ -1,6 +1,16 @@
 require("bufferline").setup {
   options = {
-    separator_style = "slant"
+    separator_style = "slant",
+    offsets = {{filetype = "NvimTree", highlight = "Directory"}},
+    custom_filter = function(buf)
+      local tab_num = 0
+      for _ in pairs(vim.api.nvim_list_tabpages()) do tab_num = tab_num + 1 end
+      if tab_num > 1 then
+          return false
+      else
+          return true
+      end
+    end
   }
 }
 
